bbfb40446c5245f2c7930f1a3ded09fe3fe251cc
BikramGyawali <gyawalibikram0@gmail.com>
2025-01-27 19:54:55 +0545

    Added new program

---
+++ Third Semester/Java/Program Section/Switch.java
@@ -0,0 +1,31 @@
+public class Switch {
+    public static void main(String[] argus) {
+        int x = 2;
+        switch (x) {
+
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wednesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+            case 6:
+                System.out.println("Friday");
+                break;
+            case 7:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid day");
+        }
+
+    }
+}
