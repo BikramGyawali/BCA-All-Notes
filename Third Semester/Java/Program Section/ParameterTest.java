3467ef1fa0c06b41db7efb27832cd9abe81a7f28
BikramGyawali <gyawalibikram0@gmail.com>
2025-03-05 23:34:07 +0545

    Learn multiple Parameter

--- Third Semester/Java/Program Section/ParameterTest.java
+++ Third Semester/Java/Program Section/ParameterTest.java
@@ -1,10 +1,11 @@
 public class ParameterTest {
-    void getNames(String name) {
-        System.out.println(name + "  Get in parameter");
+    void getNames(String name, int age) {
+        System.out.println(name + " is " + age + " year old");
     }
 
     public static void main(String[] args) {
         ParameterTest pt = new ParameterTest();
-        pt.getNames("Bikram");
+        pt.getNames("Bikram", 18);
+
     }
 }
