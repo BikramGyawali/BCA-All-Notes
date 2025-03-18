1714e2ee874a26c248e6d119af7512fa7f1760a1
BikramGyawali <gyawalibikram0@gmail.com>
2025-03-18 22:42:11 +0545

    Exception Handlign

---
+++ Third Semester/Java/Program Section/ExceptionExample.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ExceptionExample {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int a = s.nextInt();
+        System.out.println("Enter the second number");
+        int b = s.nextInt();
+        try {
+            float c = (float) a / b;
+            System.out.println("The result is " + c);
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        }
+
+    }
+}
