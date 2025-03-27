d87103f95a64b02589ea5ce99c7b0d4648f1ecc5
BikramGyawali <gyawalibikram0@gmail.com>
2025-03-27 22:48:05 +0545

    Implement the logic

--- Third Semester/Java/Program Section/Gui code/swing.java
+++ Third Semester/Java/Program Section/Gui code/swing.java
@@ -2,9 +2,14 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+// import java.awt.event.addActionListener;
 
 public class swing {
     public static void main(String[] args) {
+
+        // Starting the GUI
         JFrame f = new JFrame();
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -24,14 +29,26 @@ public class swing {
         t3.setBounds(200, 160, 80, 20);
 
         JButton b1 = new JButton("Add");
-        b1.setBounds(140, 200, 40, 30);
+        b1.setBounds(140, 200, 80, 40);
 
+        // Implementing the event handler
+
+        b1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int a = Integer.parseInt(t1.getText());
+                int b = Integer.parseInt(t2.getText());
+                int c = a + b;
+                t3.setText("a  + b " + c);
+            }
+        });
+        // added the methds to the frame
         f.add(j1);
         f.add(j2);
         f.add(j3);
         f.add(t1);
         f.add(t2);
         f.add(t3);
+        f.add(b1);
     }
 
 }
