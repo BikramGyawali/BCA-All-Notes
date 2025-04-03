088ca3d5eaacb66a11f9519b6d5cce7060e0358d
BikramGyawali <gyawalibikram0@gmail.com>
2025-04-04 00:34:00 +0545

    Series fo number

--- Third Semester/Java/Program Section/Labsheet/Series.java
+++ Third Semester/Java/Program Section/Labsheet/Series.java
@@ -4,7 +4,8 @@ public class Series {
         for (i = 1; i <= 5; i++) {
             for (j = 1; j <= i; j++) {
 
-                System.out.print(j);
+                // System.out.print(i);// for 1 22 333 ....
+                System.out.print(j); // for 1 12 123 ....
             }
             System.out.println();
         }
