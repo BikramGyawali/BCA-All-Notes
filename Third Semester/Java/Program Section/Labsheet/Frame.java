a7fc2069d78c29932ca75884ca797274b5699937
BikramGyawali <gyawalibikram0@gmail.com>
2025-04-06 22:54:24 +0545

    Complete username and password

--- Third Semester/Java/Program Section/Labsheet/Frame.java
+++ Third Semester/Java/Program Section/Labsheet/Frame.java
@@ -1,5 +1,7 @@
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextArea;
 
 public class Frame {
     public static void main(String[] args) {
@@ -10,8 +12,17 @@ public class Frame {
         f.setLayout(null);
 
         JLabel l1 = new JLabel("Username:");
-        l1.setBounds(50, 50, 100, 30);
+        l1.setBounds(90, 70, 100, 30);
+        JTextArea t1 = new JTextArea();
+        t1.setBounds(170, 74, 100, 20);
+        JLabel j2 = new JLabel("Password:");
+        j2.setBounds(90, 90, 100, 50);
+        JPasswordField t2 = new JPasswordField();
+        t2.setBounds(170, 110, 100, 20);
 
         f.add(l1);
+        f.add(t1);
+        f.add(j2);
+        f.add(t2);
     }
 }
