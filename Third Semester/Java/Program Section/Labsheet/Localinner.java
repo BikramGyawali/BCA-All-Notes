aa890890f6fc89b8c03561faee4a20ef7cd5567e
BikramGyawali <gyawalibikram0@gmail.com>
2025-04-21 23:21:11 +0545

    Complete local inner class

--- Third Semester/Java/Program Section/Labsheet/Localinner.java
+++ Third Semester/Java/Program Section/Labsheet/Localinner.java
@@ -1,18 +1,18 @@
 public class Localinner {
-    void add(int a,int b){
-        int c=a+b;
-        System.out.println("sum is"+c);
- class subtraction{
-    
-    void diff(int x,int y){
-        int d=x-y;
-        System.out.println("difference is "+d);
-       }
-       subtraction s= new subtraction();
-       s.diff(5,4);
-    }
+    void add(int a, int b) {
+        int c = a + b;
+        System.out.println("sum is" + c);
+        class subtraction {
 
- }
+            void diff(int x, int y) {
+                int d = x - y;
+                System.out.println("difference is " + d);
+            }
+        }
+        subtraction s = new subtraction();
+        s.diff(5, 4);
+
+    }
 
     public static void main(String[] args) {
         Localinner l = new Localinner();
