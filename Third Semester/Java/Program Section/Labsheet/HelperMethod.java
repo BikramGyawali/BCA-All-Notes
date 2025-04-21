1f33247b65ff44834782d8973809f4cecaecc5ea
BikramGyawali <gyawalibikram0@gmail.com>
2025-04-21 23:37:02 +0545

    Using the public

--- Third Semester/Java/Program Section/Labsheet/HelperMethod.java
+++ Third Semester/Java/Program Section/Labsheet/HelperMethod.java
@@ -1,16 +1,22 @@
 public class HelperMethod {
     public void mainmethod() {
-        int result = addition();
-        System.out.println("This is method for addition" + result);
+        // int result = addition();
+        // System.out.println("This is method for addition" + result);
+        System.out.println("This is method for addition");
     }
 
-    public int addition() {
+    public void addition() {
         int a = 10, b = 20;
-        return a + b;
+        // return a + b;
+
+        int c = a + b;
+        System.out.println(c);
+
     }
 
     public static void main(String[] args) {
         HelperMethod h = new HelperMethod();
         h.mainmethod();
+        h.addition();
     }
 }
