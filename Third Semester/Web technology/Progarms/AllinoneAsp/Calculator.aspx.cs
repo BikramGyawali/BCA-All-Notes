7b3312b2ed0504f77407e63c970f5cdbd6c32135
BikramGyawali <gyawalibikram0@gmail.com>
2025-06-25 20:18:32 +0545

    register form

--- Third Semester/Web technology/Progarms/AllinoneAsp/Calculator.aspx.cs
+++ Third Semester/Web technology/Progarms/AllinoneAsp/Calculator.aspx.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Activities.Statements;
+
 using System.Web.UI;
 
 public partial class Calculator : Page
---
+++ Third Semester/Web technology/Progarms/AllinoneAsp/RegisterForm.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+
+
+
+
+    public partial class RegisterForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    protected void btn(object sender,EventArgs e)
+    {
+        string name = t1.Text;
+        string pass = t2.Text;
+        l3.Text = "You have register   "   + name;
+    }
+
+    }
