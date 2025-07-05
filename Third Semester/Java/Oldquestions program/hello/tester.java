134bc3df74fbfd483d461099014b621b49230060
BikramGyawali <gyawalibikram0@gmail.com>
2025-07-05 09:26:50 +0545

    Done package

---
+++ Third Semester/Java/Oldquestions program/hello/tester.java
@@ -0,0 +1,12 @@
+//Define Package. Explain steps in creation and implementation of package with example.
+
+package hello;
+
+public class tester {
+    public int a = 10, b = 20;
+
+    public void display() {
+        System.out.println("This is a package");
+    }
+
+}
