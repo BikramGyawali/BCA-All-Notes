f1f2cad7fd1b4db56d9736c5b1479600bfc5cf09
BikramGyawali <gyawalibikram0@gmail.com>
2025-09-13 17:42:27 +0545

    added srtn

--- 4 Fourth Semester/Operating System/Algorithm and Programs/STRNOSBG.java
+++ 4 Fourth Semester/Operating System/Algorithm and Programs/STRNOSBG.java
@@ -14,56 +14,59 @@ public class STRNOSBG {
 		int[] remainingTime = new int[PuraProcesses];
 		int[] processStatus = new int[PuraProcesses];
 		float avgwt = 0, avgta = 0;
+
 		for (int p = 0; p < PuraProcesses; p++) {
-			System.out.println("Enter process" + (p + 1) + " Brust Time:");
+			System.out.println("Enter process" + (p + 1) + " Burst Time:");
 			burstTime[p] = sc.nextInt();
 			System.out.println("Enter process" + (p + 1) + " Release Time:");
 			releaseTime[p] = sc.nextInt();
-
+			processId[p] = p + 1;
 			remainingTime[p] = burstTime[p];
 		}
+
 		int systemTime = 0, completeProcess = 0;
 
 		while (completeProcess < PuraProcesses) {
-			int shotestKamIndex = -1;
+			int shortestKamIndex = -1;
 			int min = 9999;
+
 			for (int i = 0; i < PuraProcesses; i++) {
-				if ((releaseTime[i] <= systemTime) && releaseTime[i] > 0 && remainingTime[i] < min) {
+				if ((releaseTime[i] <= systemTime) && remainingTime[i] > 0 && remainingTime[i] < min) {
 					min = remainingTime[i];
-					shotestKamIndex = i;
+					shortestKamIndex = i;
 				}
 			}
-			if (shotestKamIndex == -1) {
+
+			if (shortestKamIndex == -1) {
 				systemTime++;
 				continue;
 			}
-			remainingTime[shotestKamIndex]--;
+
+			remainingTime[shortestKamIndex]--;
 			systemTime++;
-			if (remainingTime[shotestKamIndex] == 0) {
+
+			if (remainingTime[shortestKamIndex] == 0) {
 				completeProcess++;
-				processStatus[shotestKamIndex] = 1;
-				completionTime[shotestKamIndex] = systemTime;
-				turnAroundTime[shotestKamIndex] = completionTime[shotestKamIndex] - releaseTime[shotestKamIndex];
-				waitingTime[shotestKamIndex] = turnAroundTime[shotestKamIndex] - burstTime[shotestKamIndex];
-				avgta += turnAroundTime[shotestKamIndex];
-				avgwt += waitingTime[shotestKamIndex];
+				processStatus[shortestKamIndex] = 1;
+				completionTime[shortestKamIndex] = systemTime;
+				turnAroundTime[shortestKamIndex] = completionTime[shortestKamIndex] - releaseTime[shortestKamIndex];
+				waitingTime[shortestKamIndex] = turnAroundTime[shortestKamIndex] - burstTime[shortestKamIndex];
+				avgta += turnAroundTime[shortestKamIndex];
+				avgwt += waitingTime[shortestKamIndex];
 			}
 		}
-		System.out.println("Process ID\tRelease Time\tBrust Time\tCompletion Time\tTurn Around Time\tWaiting Time");
+
+		System.out.println("Process ID\tRelease Time\tBurst Time\tCompletion Time\tTurn Around Time\tWaiting Time");
 		for (int n = 0; n < PuraProcesses; n++) {
-			// avgwt += waitingTime[n];
-			// avgta += turnAroundTime[n];
-			int idx = processId[n] - 1; // FIX
-			System.out.println(processId[n] + "\t\t" + releaseTime[idx] + "\t\t" +
-					burstTime[idx] + "\t\t" + completionTime[idx] + "\t\t" +
-					turnAroundTime[idx] + "\t\t\t" + waitingTime[idx]);
+			System.out.println(processId[n] + "\t\t" + releaseTime[n] + "\t\t" +
+					burstTime[n] + "\t\t" + completionTime[n] + "\t\t" +
+					turnAroundTime[n] + "\t\t\t" + waitingTime[n]);
 		}
 
-		System.out.println("\n Average Turn Around Time: " + (float) (avgta / PuraProcesses));
-		System.out.println("\n Average Waiting  Time: " + (float) (avgwt / PuraProcesses));
+		System.out.println("\nAverage Turn Around Time: " + (avgta / PuraProcesses));
+		System.out.println("Average Waiting Time: " + (avgwt / PuraProcesses));
+
 		sc.close();
-		for (int i = 0; i < PuraProcesses; i++) {
-			System.out.print("| P" + processId[i] + " ");
-		}
+
 	}
 }
