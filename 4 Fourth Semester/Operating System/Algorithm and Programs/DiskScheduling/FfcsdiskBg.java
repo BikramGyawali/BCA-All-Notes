ff5626979e9e6084b069f9c0fbe711ac76864a85
BikramGyawali <gyawalibikram0@gmail.com>
2025-09-22 11:11:18 +0545

    added fcfs

---
+++ 4 Fourth Semester/Operating System/Algorithm and Programs/DiskScheduling/FfcsdiskBg.java
@@ -0,0 +1,27 @@
+package DiskScheduling;
+
+import java.util.Scanner;
+
+public class FfcsdiskBg {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of requests:");
+		int binti = sc.nextInt();
+		int requests[] = new int[binti];
+		System.out.println("Enter the disk sequence:");
+		for (int b = 0; b < binti; b++) {
+			requests[b] = sc.nextInt();
+		}
+		System.out.println("Enter the initial head position:");
+		int suruPosition = sc.nextInt();
+		int totalHeadMovement = 0;
+		int ailekoPosition = suruPosition;
+		System.out.println("Disk head movements:");
+		for (int m = 0; m < binti; m++) {
+			System.out.println("Moving From " + ailekoPosition + " to " + requests[m]);
+			totalHeadMovement += Math.abs(ailekoPosition - requests[m]);
+			ailekoPosition = requests[m];
+		}
+		System.out.println("Total head movement is: " + totalHeadMovement);
+	}
+}
