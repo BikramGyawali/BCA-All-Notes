c371047f49baebb0e5162238895d15ae55699eb5
BikramGyawali <gyawalibikram0@gmail.com>
2025-09-22 11:41:08 +0545

    added shortest seek time next

---
+++ 4 Fourth Semester/Operating System/Algorithm and Programs/DiskScheduling/SSTNdiskBg.java
@@ -0,0 +1,40 @@
+package DiskScheduling;
+
+import java.util.Scanner;
+
+public class SSTNdiskBg {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of requests:");
+		int binti = sc.nextInt();
+		int requests[] = new int[binti];
+		System.out.println("Enter the disk sequence:");
+		for (int b = 0; b < binti; b++) {
+			requests[b] = sc.nextInt();
+		}
+		System.out.println("Enter the initial head position:");
+		int suruPosition = sc.nextInt();
+		boolean visited[] = new boolean[binti];
+		int totalHeadMovement = 0;
+		int ailekoPosition = suruPosition;
+		System.out.println("Disk head movements:");
+		for (int m = 0; m < binti; m++) {
+			int thoriiDistance = Integer.MAX_VALUE;
+			int index = -1;
+			for (int j = 0; j < binti; j++) {
+				if (!visited[j]) {
+					int distance = Math.abs(ailekoPosition - requests[j]);
+					if (distance < thoriiDistance) {
+						thoriiDistance = distance;
+						index = j;
+					}
+				}
+			}
+			visited[index] = true;
+			System.out.println("Moving From " + ailekoPosition + " to " + requests[index]);
+			totalHeadMovement += thoriiDistance;
+			ailekoPosition = requests[index];
+		}
+		System.out.println("Total head movement is: " + totalHeadMovement);
+	}
+}
