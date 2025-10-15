8ee22d4d09fe5660649fd0647268581187459199
BikramGyawali <gyawalibikram0@gmail.com>
2025-10-15 11:07:40 +0545

    BAnkers

--- 4 Fourth Semester/Operating System/Algorithm and Programs/DeadLock/BankersBG.java
+++ 4 Fourth Semester/Operating System/Algorithm and Programs/DeadLock/BankersBG.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
 public class BankersBG {
-	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of processes");
+		int processNum= sc.nextInt();
+		System.out.println("Enter the number of resources");
+		int resourceNum= sc.nextInt();
+
+		int[][] max = new int[processNum][resourceNum];
+		int[][] allocation = new int[processNum][resourceNum];
+		int[][] chiyeko = new int[processNum][resourceNum];
+		int[] vako = new int[resourceNum];
+		System.out.println("Enter the max matrix");
+		for(int i=0;i<processNum;i++) {
+			for(int j=0;j<resourceNum;j++) {
+				max[i][j]=sc.nextInt();
+			}
+		}
+	}
 }
