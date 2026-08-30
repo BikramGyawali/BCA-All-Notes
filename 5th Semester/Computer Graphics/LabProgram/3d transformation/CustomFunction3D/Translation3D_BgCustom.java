38392bbea2b77674be17b6c82bd9b190dd4fb9c4
BikramGyawali <gyawalibikram0@gmail.com>
2026-08-31 00:07:05 +0545

    Changes code

---
+++ 5th Semester/Computer Graphics/LabProgram/3d transformation/CustomFunction3D/Translation3D_BgCustom.java
@@ -0,0 +1,49 @@
+package CustomFunction3D;
+
+import package3D.BGhelperPackage3D;
+import java.util.Scanner;
+
+public class Translation3D_BgCustom {
+	public double[][] BGTranslation(double[][] BGOriginal, double Tx, double Ty, double Tz) {
+		double[][] BGTransformed = new double[8][3];
+
+		for (int Bikram = 0; Bikram < 8; Bikram++) {
+			BGTransformed[Bikram][0] = BGOriginal[Bikram][0] + Tx;
+			BGTransformed[Bikram][1] = BGOriginal[Bikram][1] + Ty;
+			BGTransformed[Bikram][2] = BGOriginal[Bikram][2] + Tz;
+		}
+
+		return BGTransformed;
+	}
+
+	public void BGHehe() {
+		Scanner sa = new Scanner(System.in);
+		BGhelperPackage3D bg = new BGhelperPackage3D();
+
+		bg.inputCoordinate(sa);
+
+		System.out.print("Enter Translation Along X-Axis: ");
+		double Tx = sa.nextDouble();
+
+		System.out.print("Enter Translation Along Y-Axis: ");
+		double Ty = sa.nextDouble();
+
+		System.out.print("Enter Translation Along Z-Axis: ");
+		double Tz = sa.nextDouble();
+
+		double[][] BGOriginal = bg.getOriginalPoint();
+
+		double[][] BGTransformed = BGTranslation(BGOriginal, Tx, Ty, Tz);
+
+		bg.showOriginalCoordinate(BGOriginal);
+		bg.showTransformedCoordinate(BGTransformed);
+
+		BGhelperPackage3D.CustomPanel3D AshliPanel = new BGhelperPackage3D.CustomPanel3D(BGOriginal, BGTransformed);
+
+		BGhelperPackage3D.createFrame("3D Translation - Custom Function", AshliPanel);
+	}
+
+	public static void main(String[] args) {
+		new Translation3D_BgCustom().BGHehe();
+	}
+}
---
+++ 5th Semester/Computer Graphics/LabProgram/3d transformation/package3D/BGhelperPackage3D.java
@@ -0,0 +1,284 @@
+package package3D;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Scanner;
+
+public class BGhelperPackage3D {
+    public double Xa,Ya,Za;
+    public double Xb,Yb,Zb;
+    public double Xc,Yc,Zc;
+    public double Xd,Yd,Zd;
+    public double Xe,Ye,Ze;
+    public double Xf,Yf,Zf;
+    public double Xg,Yg,Zg;
+    public double Xh,Yh,Zh;
+
+    public void inputCoordinate(Scanner sa) {
+        System.out.println("Enter 8 Coordinates for Ghan (Cube):");
+
+        System.out.print("A - Enter garnus X,Y,Z: ");
+        Xa=sa.nextDouble();
+        Ya=sa.nextDouble();
+        Za=sa.nextDouble();
+
+        System.out.print("B - Enter garnus X,Y,Z: ");
+        Xb=sa.nextDouble();
+        Yb=sa.nextDouble();
+        Zb=sa.nextDouble();
+
+        System.out.print("C - Enter garnus X,Y,Z: ");
+        Xc=sa.nextDouble();
+        Yc=sa.nextDouble();
+        Zc=sa.nextDouble();
+
+        System.out.print("D - Enter garnus X,Y,Z: ");
+        Xd=sa.nextDouble();
+        Yd=sa.nextDouble();
+        Zd=sa.nextDouble();
+
+        System.out.print("E - Enter garnus X,Y,Z: ");
+        Xe=sa.nextDouble();
+        Ye=sa.nextDouble();
+        Ze=sa.nextDouble();
+
+        System.out.print("F - Enter garnus X,Y,Z: ");
+        Xf=sa.nextDouble();
+        Yf=sa.nextDouble();
+        Zf=sa.nextDouble();
+
+        System.out.print("G - Enter garnus X,Y,Z: ");
+        Xg=sa.nextDouble();
+        Yg=sa.nextDouble();
+        Zg=sa.nextDouble();
+
+        System.out.print("H - Enter garnus X,Y,Z: ");
+        Xh=sa.nextDouble();
+        Yh=sa.nextDouble();
+        Zh=sa.nextDouble();
+    }
+
+    public double[][] getOriginalPoint() {
+        return new double[][]{
+            {Xa,Ya,Za},
+            {Xb,Yb,Zb},
+            {Xc,Yc,Zc},
+            {Xd,Yd,Zd},
+            {Xe,Ye,Ze},
+            {Xf,Yf,Zf},
+            {Xg,Yg,Zg},
+            {Xh,Yh,Zh}
+        };
+    }
+
+    public void showOriginalCoordinate(double[][] BGPoint) {
+        char[] BGName={'A','B','C','D','E','F','G','H'};
+
+        System.out.println("\nOriginal Coordinates:");
+
+        for(int i=0;i<8;i++) {
+            System.out.println(
+                BGName[i]+" = ("+
+                BGPoint[i][0]+", "+
+                BGPoint[i][1]+", "+
+                BGPoint[i][2]+")"
+            );
+        }
+    }
+
+    public void showTransformedCoordinate(double[][] BGPoint) {
+        char[] BGName={'A','B','C','D','E','F','G','H'};
+
+        System.out.println("\nTransformed Coordinates:");
+
+        for(int i=0;i<8;i++) {
+            System.out.println(
+                BGName[i]+"' = ("+
+                BGPoint[i][0]+", "+
+                BGPoint[i][1]+", "+
+                BGPoint[i][2]+")"
+            );
+        }
+    }
+
+    public static void createFrame(String BGTitle,JPanel AshliPanel) {
+        JFrame BGF=new JFrame(BGTitle);
+
+        BGF.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        BGF.setSize(1000,750);
+        BGF.setLocationRelativeTo(null);
+
+        BGF.add(AshliPanel);
+        BGF.setVisible(true);
+    }
+
+    public static class CustomPanel3D extends JPanel {
+        private double[][] BGOriginal;
+        private double[][] BGTransformed;
+
+        public CustomPanel3D(double[][] BGOriginal,double[][] BGTransformed) {
+            this.BGOriginal=BGOriginal;
+            this.BGTransformed=BGTransformed;
+            setBackground(Color.WHITE);
+        }
+
+        public int BGScreenX(double XCoordinate,double ZCoordinate) {
+            return (int)Math.round(
+                getWidth()/2+
+                XCoordinate*35-
+                ZCoordinate*18
+            );
+        }
+
+        public int BGScreenY(double YCoordinate,double ZCoordinate) {
+            return (int)Math.round(
+                getHeight()/2-
+                YCoordinate*35+
+                ZCoordinate*18
+            );
+        }
+
+        public void BGDrawAxes(Graphics2D g2) {
+            int centerX=getWidth()/2;
+            int centerY=getHeight()/2;
+
+            g2.setColor(Color.BLACK);
+            g2.setStroke(new BasicStroke(2));
+
+            g2.drawLine(0,centerY,getWidth(),centerY);
+            g2.drawString("X-Axis",getWidth()-60,centerY-10);
+
+            g2.drawLine(centerX,0,centerX,getHeight());
+            g2.drawString("Y-Axis",centerX+10,25);
+
+            g2.drawLine(
+                centerX,
+                centerY,
+                centerX-200,
+                centerY+200
+            );
+
+            g2.drawString(
+                "Z-Axis",
+                centerX-230,
+                centerY+210
+            );
+
+            g2.drawString(
+                "O(0,0,0)",
+                centerX+8,
+                centerY+18
+            );
+        }
+
+        public void BGDrawCube(
+            Graphics2D g2,
+            double[][] BGPoint,
+            Color BGColor,
+            boolean BGIsTransformed
+        ) {
+            int[][] BGEdge={
+                {0,1},{1,2},{2,3},{3,0},
+                {4,5},{5,6},{6,7},{7,4},
+                {0,4},{1,5},{2,6},{3,7}
+            };
+
+            char[] BGName={
+                'A','B','C','D',
+                'E','F','G','H'
+            };
+
+            g2.setColor(BGColor);
+            g2.setStroke(new BasicStroke(2));
+
+            for(int[] BGLine:BGEdge) {
+                int BGFirst=BGLine[0];
+                int BGSecond=BGLine[1];
+
+                int X1=BGScreenX(
+                    BGPoint[BGFirst][0],
+                    BGPoint[BGFirst][2]
+                );
+
+                int Y1=BGScreenY(
+                    BGPoint[BGFirst][1],
+                    BGPoint[BGFirst][2]
+                );
+
+                int X2=BGScreenX(
+                    BGPoint[BGSecond][0],
+                    BGPoint[BGSecond][2]
+                );
+
+                int Y2=BGScreenY(
+                    BGPoint[BGSecond][1],
+                    BGPoint[BGSecond][2]
+                );
+
+                g2.drawLine(X1,Y1,X2,Y2);
+            }
+
+            for(int i=0;i<8;i++) {
+                int X=BGScreenX(
+                    BGPoint[i][0],
+                    BGPoint[i][2]
+                );
+
+                int Y=BGScreenY(
+                    BGPoint[i][1],
+                    BGPoint[i][2]
+                );
+
+                g2.fillOval(X-4,Y-4,8,8);
+
+                String BGLabel=
+                    BGIsTransformed
+                    ?BGName[i]+"'"
+                    :String.valueOf(BGName[i]);
+
+                g2.drawString(
+                    BGLabel,
+                    X+6,
+                    Y-6
+                );
+            }
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+            Graphics2D g2=(Graphics2D)g;
+
+            BGDrawAxes(g2);
+
+            BGDrawCube(
+                g2,
+                BGOriginal,
+                Color.BLUE,
+                false
+            );
+
+            BGDrawCube(
+                g2,
+                BGTransformed,
+                Color.RED,
+                true
+            );
+
+            g2.setColor(Color.BLUE);
+            g2.drawString(
+                "Original Cube: A,B,C,D,E,F,G,H",
+                20,
+                30
+            );
+
+            g2.setColor(Color.RED);
+            g2.drawString(
+                "Transformed Cube: A',B',C',D',E',F',G',H'",
+                20,
+                55
+            );
+        }
+    }
+}
